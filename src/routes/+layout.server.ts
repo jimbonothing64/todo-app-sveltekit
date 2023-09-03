3d247d959802ed75d5c21291e08aaba36242620a
James Napier <jamesnapier64@gmail.com>
2023-09-03 17:09:04 +1200

    remove login/signin for auth user

---
+++ src/routes/+layout.server.ts
@@ -0,0 +1,11 @@
+import type { LayoutServerLoad } from './$types';
+import { redirect } from '@sveltejs/kit';
+
+export const load: LayoutServerLoad = async ({ locals }) => {
+	const session = await locals.auth.validate();
+	if (!session) throw redirect(302, '/login');
+	return {
+		userId: session.user.userId,
+		username: session.user.username
+	};
+};
